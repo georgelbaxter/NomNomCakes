c8ed52a91bbb0fed808da9b8c20b17bc48b5a408
unknown <ithomson0457@2A207-B04.conestogac.on.ca>
2016-03-29 12:13:54 -0400

    added description

--- Models/Cake.cs
+++ Models/Cake.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +9,6 @@ namespace Models
     public class Cake
     {
         public int CakeID { get; set;}
+        public string CakeDescription { get; set;}
     }
 }
