bcdf1983b8548ff241fbf9f93010055c8e6c3140
georgelbaxter <Gbaxter2031@conestogac.on.ca>
2016-03-29 12:02:05 -0400

    Initial commit
    Empty project for an ASP.NET class project

---
+++ WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NomNomCakes.MvcApplication" Language="C#" %>
---
+++ WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NomNomCakes.Startup))]
+namespace NomNomCakes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
