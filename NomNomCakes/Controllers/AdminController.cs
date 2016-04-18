a1b7b0099e9cc3bb7b3804df3f4ee07ef26c1c3a
unknown <qzhou3254@2A501-E04.conestogac.on.ca>
2016-04-18 10:46:50 -0400

    basic controller functions

--- NomNomCakes/Controllers/AdminController.cs
+++ NomNomCakes/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 using Contracts;
 using Models;
+using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,26 +11,51 @@ namespace NomNomCakes.Controllers
 {
     public class AdminController : Controller
     {
-        IRepositoryBase<Cake> cakes;
-        IRepositoryBase<Icing> icings;
-        IRepositoryBase<Topping> toppings;
-        public AdminController(IRepositoryBase<Cake> cakes, IRepositoryBase<Icing> icings, IRepositoryBase<Topping> toppings)
+        public class HomeController : Controller
         {
-            this.cakes = cakes;
-            this.icings = icings;
-            this.toppings = toppings;
-        }
+            IRepositoryBase<Cake> cakes;
+            IRepositoryBase<Icing> icings;
+            IRepositoryBase<Topping> toppings;
+            IRepositoryBase<Basket> baskets;
+            IRepositoryBase<BasketItem> basketItem;
+            IRepositoryBase<Coupon> coupons;
+            IRepositoryBase<CouponType> couponTypes;
+            IRepositoryBase<BasketCoupon> basketCoupons;
+            BasketService basketService;
+            public HomeController(IRepositoryBase<Cake> cakes, IRepositoryBase<Icing> icings, IRepositoryBase<Topping> toppings, IRepositoryBase<BasketItem> basketItem, IRepositoryBase<Basket> baskets, IRepositoryBase<Coupon> coupons, IRepositoryBase<CouponType> couponTypes, IRepositoryBase<BasketCoupon> basketCoupons)
+            {
+                this.cakes = cakes;
+                this.icings = icings;
+                this.toppings = toppings;
+                this.baskets = baskets;
+                this.basketItem = basketItem;
+                this.coupons = coupons;
+                this.couponTypes = couponTypes;
+                this.basketCoupons = basketCoupons;
+                basketService = new BasketService(this.baskets, this.coupons, this.basketCoupons, this.couponTypes);
+            }
 
-        // GET: Admin
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult CakeList()
-        {
-            var model = cakes.GetAll();
-            return View(model);
-        }
+            // GET: Admin
+            public ActionResult Index()
+            {
+                return View();
+            }
+            public ActionResult CakeList()
+            {
+                var model = cakes.GetAll();
+                return View(model);
+            }
+            public ActionResult ToppingList()
+            {
+                var model = toppings.GetAll();
+                return View(model);
+            }
+            public ActionResult IcingList()
+            {
+                var model = icings.GetAll();
+                return View(model);
+            }
 
+        }
     }
 }
